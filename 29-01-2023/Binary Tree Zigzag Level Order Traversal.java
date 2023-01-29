5b706fe4a27ae485a027592a59a3ca6acd4fdc50
Revanthvarma Odugu <84526293+Revanth-Varma@users.noreply.github.com>
2023-01-29 14:52:49 +0530

    Leetcode103

---
+++ 29-01-2023/Binary Tree Zigzag Level Order Traversal.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        boolean isForward = true;
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> row = new LinkedList<>();
+            for(int i=0;i<size;i++){
+                TreeNode curr = queue.poll();
+                if(curr.left!=null){
+                    queue.add(curr.left);
+                }
+                if(curr.right!=null){
+                    queue.add(curr.right);
+                }
+                if(isForward){
+                    row.add(curr.val);
+                }
+                else{
+                    row.add(0,curr.val);
+                }
+            }
+            ans.add(row);
+            isForward = !isForward;
+        }
+        return ans;
+    }
+}
